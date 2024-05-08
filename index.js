65a067e3b153f5b24840080a30a5c0cd0673fd86
wfeychuijs <js1leitoosg@hotmail.com>
2024-05-08 12:44:04 +0700

    init project

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const count = Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 97]++;
-    count[t.charCodeAt(i) - 97]--;
-  }
-  return count.every((c) => c === 0);
-}
+// Fund Alice and sponsor accounts
+await aptos.fundAccount({
+  accountAddress: aliceAddress,
+  amount: ALICE_INITIAL_BALANCE,
+});
+await aptos.fundAccount({
+  accountAddress: sponsorAddress,
+  amount: SPONSOR_INITIAL_BALANCE,
+});
+
+// Show account balances
+const aliceBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+});
+const sponsorBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
