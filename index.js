b65cf38152eddaa14d9452a06b27a7c7240bd88c
wfeychuijs <js1leitoosg@hotmail.com>
2024-05-11 04:59:54 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
+function subarraySum(nums, k) {
+  const map = new Map();
+  map.set(0, 1);
+  let count = 0;
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    map.set(sum, (map.get(sum) || 0) + 1);
   }
-  return i === s.length;
+  return count;
 }
